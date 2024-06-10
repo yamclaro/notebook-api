03d99dd6fdb43739b9c5763f1246f1223f5175ef
yamclaro <yam.claro2019@gmail.com>
2024-06-10 00:03:24 -0400

    feat# includ retorno

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,22 @@
 class Contact < ApplicationRecord
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+   belongs_to :kind 
+   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
     def author
        "yam kevim"
     end
+
+    def kind_description
+      self.kind.description
+    end
     
     def as_json(options={})
-       super(methods: :author, root:true)
+       super( 
+         root:true,
+         methods: [:author, :kind_description],
+         except: :kind_id,
+         include: { kind: { only: [:id, :description]}}
+       )
     end
 end
 
