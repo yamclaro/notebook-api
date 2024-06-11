c19f2f271da8dd9744e0dc20fe254f6af76b71f0
yamclaro <yam.claro2019@gmail.com>
2024-06-11 10:53:37 -0400

    feat# implementacao de autenticação http basica

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+      #include ActionController::HttpAuthentication::Basic::ControllerMethods
+      #http_basic_authenticate_with name: "username", password: "passwd123"
 end
